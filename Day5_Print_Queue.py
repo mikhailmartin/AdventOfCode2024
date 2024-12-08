89560068ba7b22f292770facbca781896e493448
Mikhail Martin <JohnDoe110@yandex.ru>
2024-12-08 03:22:48 +0300

    Day 5 part 2 done

--- Day5_Print_Queue.py
+++ Day5_Print_Queue.py
@@ -3,34 +3,55 @@ import os
 INPUT_DATA_PATH = os.path.join("data", "day5.txt")
 
 
-def part1(rules: list[str], pages) -> int:
+def main(rules: list[str], pages) -> tuple[int, int]:
 
-    graph = dict()  # граф связей
+    graph = dict()  # граф связей типа dst: [src, ..]
     for rule in rules:
         # print(rule)
-        key, value = map(int, rule.split("|"))
-        if key not in graph:
-            graph[key] = []
-        graph[key].append(value)
+        src, dst = map(int, rule.split("|"))
+        if dst not in graph:
+            graph[dst] = []
+        graph[dst].append(src)
 
-    counter = 0
+    part1_result = 0
+    part2_result = 0
     for page in pages:
         page = list(page)
         if is_in_right_order(page, graph):
-            counter += page[len(page) // 2]
+            part1_result += get_middle(page)
+        else:
+            page = order(page, graph)
+            part2_result += get_middle(page)
 
-    return counter
+    return part1_result, part2_result
 
 
 def is_in_right_order(page: list[int], graph: dict[int, list[int]]) -> bool:
 
     flag = True
-    for i in range(len(page) - 1):
-        if page[i] in graph[page[i+1]]:
+    for i in range(1, len(page)):
+        if page[i] in graph[page[i-1]]:
             flag = False
             break
 
     return flag
+
+
+def order(page: list[int], graph: dict[int, list[int]]) -> list[int]:
+
+    i = 1
+    while i != len(page):
+        if page[i] in graph[page[i-1]]:
+            page[i], page[i-1] = page[i-1], page[i]
+            i = max(1, i-1)
+        else:
+            i += 1
+
+    return page
+
+
+def get_middle(lst: list):
+    return lst[len(lst) // 2]
 
 
 if __name__ == "__main__":
@@ -39,6 +60,7 @@ if __name__ == "__main__":
     rules = rules.split("\n")
     pages = map(lambda x: map(int, x.split(",")), pages.split("\n"))
 
-    part1_result = part1(rules, pages)
+    part1_result, part2_result = main(rules, pages)
 
     print(part1_result)
+    print(part2_result)
