d245c1c507aeb7efc64f6879cfb8d533eab26729
Mikhail Martin <JohnDoe110@yandex.ru>
2024-12-10 03:28:42 +0300

    Day8 part 2 done

--- Day8_Resonant_Collinearity.py
+++ Day8_Resonant_Collinearity.py
@@ -35,6 +35,52 @@ def part1(matrix) -> int:
     return len(antinodes)
 
 
+def part2(matrix) -> int:
+
+    max_rows, max_cols = matrix.shape
+
+    antennas = {}
+    for x, y in product(range(max_rows), range(max_cols)):
+        content = matrix[x, y]
+        if content == ".":
+            continue
+        if content not in antennas:
+            antennas[content] = []
+        antennas[content].append((x, y))
+
+    antinodes = set()
+    for ants in antennas.values():
+        for antenna1, antenna2 in permutations(ants, r=2):
+
+            smth = foo(antenna1, antenna2, max_rows, max_cols)
+            antinodes = antinodes | smth
+
+    return len(antinodes)
+
+
+def foo(antenna1, antenna2, max_rows, max_cols):
+
+    antinodes = {antenna1, antenna2}
+
+    # в одну сторону
+    dx = antenna2[0] - antenna1[0]
+    dy = antenna2[1] - antenna1[1]
+    antinode = antenna2
+    while is_inside_map(antinode, max_rows, max_cols):
+        antinodes.add(antinode)
+        antinode = antinode[0] + dx, antinode[1] + dy
+
+    # в другую сторону
+    dx = antenna1[0] - antenna2[0]
+    dy = antenna1[1] - antenna2[1]
+    antinode = antenna1
+    while is_inside_map(antinode, max_rows, max_cols):
+        antinodes.add(antinode)
+        antinode = antinode[0] + dx, antinode[1] + dy
+
+    return antinodes
+
+
 def symmetrical(point: tuple[int, int], axis: tuple[int, int]) -> tuple[int, int]:
 
     x = 2 * axis[0] - point[0]
@@ -57,5 +103,7 @@ if __name__ == "__main__":
     matrix = np.array(matrix)
 
     part1_result = part1(matrix)
+    part2_result = part2(matrix)
 
     print(part1_result)
+    print(part2_result)
