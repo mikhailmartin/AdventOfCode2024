5a1a3cc91408fb30b038c7c20f4d76bd04103887
Mikhail Martin <JohnDoe110@yandex.ru>
2024-12-19 00:43:24 +0300

    Day 9 part 2 in progress

--- Day09_Disk_Fragmenter.py
+++ Day09_Disk_Fragmenter.py
@@ -57,53 +57,35 @@ def part1(disk_map: str) -> int:
 
 def part2(disk_map: str) -> int:
 
+    files = []
     free_spaces = []
-    pointer_l = 0
-    id_num_l, is_file_l, block_size_l = read(pointer_l, disk_map)
-    pointer_r = len(disk_map) - 1
-    id_num_r, is_file_r, block_size_r = read(pointer_r, disk_map)
-    checksum = 0
-    index = 0
-    while pointer_r != pointer_l:
-
-        # если справа файл, то пытаемся ему найти место слева
-        if is_file_r:
-            # ищем место в кусках
-            if free_spaces:
-                for block_size, start_index in free_spaces:
-                    if block_size
-            # если слева файл, то просто считаем по нему контрольную сумму
-            if is_file_l:
-                for _ in range(block_size_l):
-                    checksum += index * id_num_l
-                    index += 1
-                pointer_l += 1
-                id_num_l, is_file_l, block_size_l = read(pointer_l, disk_map)
-            # если слева свободное пространство,
-            else:
-                # если можем в него засунуть файл справа,
-                if block_size_l >= block_size_r:
-                    # то засовываем
-                    while block_size_r > 0:
-                        checksum += index * id_num_r
-                        index += 1
-                        block_size_l -= 1
-                        block_size_r -= 1
-                    # если закончилось свободное пространство слева
-                    if block_size_l == 0:
-                        pointer_l += 1
-                        id_num_l, is_file_l, block_size_l = read(pointer_l, disk_map)
-                    # если остался свободный кусок пространства слева
-                    elif block_size_l > 0:
-                        free_spaces.append((block_size_l, index))
-
-                    pointer_r -= 1
-                    id_num_r, is_file_r, block_size_r = read(pointer_r, disk_map)
-
-        # если справа пустое пространство, то пропускаем его
+    position = 0
+    for j, number in enumerate(disk_map):
+        id_number, is_file, block_size = read(j, disk_map)
+        if is_file:
+            files.append((position, block_size, id_number))
         else:
-            pointer_r -= 1
-            id_num_r, is_file_r, block_size_r = read(pointer_r, disk_map)
+            free_spaces.append((position, block_size))
+        position += block_size
+
+    l = len(free_spaces)
+    checksum = 0
+    for i, (position_r, block_size_r, id_number_r) in enumerate(reversed(files)):
+        for j, (position_l, block_size_l) in enumerate(free_spaces[:l-1-i]):
+            # если свободное пространство может вместить файл,
+            if block_size_l >= block_size_r:
+                # вмещаем файл
+                for _ in range(block_size_r):
+                    checksum += position_l * id_number_r
+                    position_l += 1
+                    block_size_l -= 1
+                free_spaces[j] = (position_l, block_size_l)
+                break
+        # если же не нашли свободного пространства
+        else:
+            for _ in range(block_size_r):
+                checksum += position_r * id_number_r
+                position_r += 1
 
     return checksum
 
@@ -122,5 +104,7 @@ if __name__ == "__main__":
     disk_map = open(INPUT_DATA_PATH, "r").read().strip()
 
     part1_result = part1(disk_map)
+    part2_result = part2(disk_map)
 
     print(part1_result)
+    print(part2_result)
